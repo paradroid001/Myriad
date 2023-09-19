45f9c130b6533b419fb36f10207a7f33e973d847
paradroid001 <brad.power@gmail.com>
2023-09-19 20:34:07 +0800

    Adding spdlog

--- Engine/src/myriad.h
+++ Engine/src/myriad.h
@@ -1,9 +1,9 @@
 #pragma once
 
 // For client applications only, not for engine to include
-#include "../core/MyrApplication.h"
+#include "core/MyrApplication.h"
 #include "stdio.h"
 
 // --- Entry Point ---
-#include "../core/MyrEntryPoint.h"
+#include "core/MyrEntryPoint.h"
 // -------------------
