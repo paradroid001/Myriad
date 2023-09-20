958c9234b2a279bf5be94dd091216613246870d2
paradroid001 <brad.power@gmail.com>
2023-09-20 12:14:35 +0800

    Move spdlog

---
+++ Engine/src/core/Log.cpp
@@ -0,0 +1,9 @@
+#include "Log.h"
+#include "spdlog/spdlog.h"
+
+namespace Myriad
+{
+    Log::Log() {}
+
+    Log::~Log() {}
+} // namespace Myriad
---
+++ Engine/src/core/Log.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "core.h"
+
+namespace Myriad
+{
+    class MYR_API Log
+    {
+        Log();
+        virtual ~Log();
+    };
+} // namespace Myriad
