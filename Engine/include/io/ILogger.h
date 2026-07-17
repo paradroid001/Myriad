2b23678a845750aea6c22eb4935373ad9b4978ba
paradroid001 <brad.power@gmail.com>
2026-07-17 13:37:41 +0800

    An ILogger file that was left behind

--- Engine/include/io/ILogger.h
+++ Engine/include/io/ILogger.h
@@ -6,23 +6,26 @@
 
 namespace Myriad
 {
-  typedef enum MyrLogLevel_t
-  {
-    MYR_LOGLEVEL_TRACE = 0,
-    MYR_LOGLEVEL_INFO = 2,
-    MYR_LOGLEVEL_WARNING = 4,
-    MYR_LOGLEVEL_ERROR = 8,
-    MYR_LOGLEVEL_CRITICAL = 16,
-  } MyrLogLevel_t;
+    typedef enum MyrLogLevel_t
+    {
+        MYR_LOGLEVEL_TRACE = 0,
+        MYR_LOGLEVEL_INFO = 2,
+        MYR_LOGLEVEL_WARNING = 4,
+        MYR_LOGLEVEL_ERROR = 8,
+        MYR_LOGLEVEL_CRITICAL = 16,
+    } MyrLogLevel_t;
 
-  class MYR_API ILogger
-  {
-  protected:
-    MyrLogLevel_t log_level_;
+    class MYR_API ILogger
+    {
+      protected:
+        MyrLogLevel_t log_level_;
 
-  public:
-    inline virtual void SetLogLevel(MyrLogLevel_t logLevel) { log_level_ = logLevel; }
-    virtual void Log(MyrLogLevel_t loglevel, const char *fmt, ...) = 0;
-  };
-}
+      public:
+        inline virtual void SetLogLevel(MyrLogLevel_t logLevel)
+        {
+            log_level_ = logLevel;
+        }
+        virtual void Log(MyrLogLevel_t loglevel, const char *fmt, ...) = 0;
+    };
+} // namespace Myriad
 #endif
